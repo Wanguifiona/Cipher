92d18f812b3b8fd5b94d30b701f5689fe2640a43
Fiona Wangui <91875682+Wanguifiona@users.noreply.github.com>
2021-12-06 21:28:51 +0300

    add .gitignore

--- src/test/java/model/DecryptionTest.java
+++ src/test/java/model/DecryptionTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DecryptionTest {
+    @Test
+    public void decryption_allUpperCase_String(){
+        decryption newdecryption = new decryption();
+    }
 
 
 }
