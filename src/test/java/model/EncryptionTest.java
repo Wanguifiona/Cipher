14d3d5d86848fec3a3f8e8d4682d9f7da3c5ede3
Fiona Wangui <91875682+Wanguifiona@users.noreply.github.com>
2021-12-06 20:41:24 +0300

    add encryption test

--- src/test/java/model/EncryptionTest.java
+++ src/test/java/model/EncryptionTest.java
@@ -1,2 +1,17 @@
 package model;
 
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EncryptionTest {
+    @Test
+    public void encryption_allUpperCase_String() {
+        Encryption newEncryption = new Encryption();
+        String oMessage = "CEASAR CIPHER";
+        assertEquals(oMessage, Encryption.encryption("ceasar cipher", 1));
+
+
+    }
+}
